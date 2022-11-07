a09a54b08701b58084ca668fdb6940b125d26132
RishikaSaini1234 <106665787+RishikaSaini1234@users.noreply.github.com>
2022-11-07 19:14:15 +1000

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -24,7 +24,7 @@ function getData() {
                   Room_names = childKey;
                   //Start code
                   console.log("Room Name -" + Room_names);
-                  row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)'>#" + Room_name + "</div><hr>";
+                  row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div><hr>";
                   document.getElementById("output").innerHTML += row;
 
                   //End code
